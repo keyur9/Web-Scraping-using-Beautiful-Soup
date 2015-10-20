cacd7f290efc94ec30c3e5feeed790f3540fe794
keyur9 <keyur.kv@gmail.com>
2015-10-20 01:57:00 -0400

    Web Scraping using Beautiful Soup
    Script to scrape websites using Beautiful Soup

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Oct 05 12:01:12 2015
+Created on Tue Oct 20 00:48:45 2015
 @author: keyur
 
 ### Name: Keyur Doshi
@@ -21,175 +21,91 @@ Step 3: Store all the reviews from all 4 websites in a single file called "revie
 : the FULL review text, exactly as it appears on the website.
 : the review's rating 
 : the review's date of submission, as it appears on the website.
-
 """
-#Importing libraries
-import sys,time,socket
-from bs4 import BeautifulSoup
-import mechanize
-from urllib2 import *
-import urllib2
 
-#Setting socket timeout
-socket.setdefaulttimeout(20)
+#Importing library
+import sys,time,urllib2
+from bs4 import BeautifulSoup
 
 #Capturing the start time of this program
 start_time = time.clock()
 
-#Mechanize the browser
-br = mechanize.Browser()
+#Website used for parsing
+website = str('flipkart.com')
 
-
+#Initializing browser
 browser=urllib2.build_opener()
 browser.addheaders=[('User-agent', 'Mozilla/5.0')]
 
 #create a new file and open a connection to it.
-#fileWriter=open('reviewk3.txt','w')
-fileWriter=open('reviews.txt','a')
+fileWriter=open('reviews.txt','w')
 fileReader=open('in.txt')
 
+#Initializing index
+index=0
+
 #Creating the list to store all the records 
-nameList = list()
-DateList = list()
-ReviewList = list()
-RatingList = list()
-
-#Site used for parsing
-sitename = 'www.flipkart.com'
-
-#Setting the read timeout
-read_timeout = 1.0
+ReviewList = []
+RatingList=[]
+DateList=[]
 
 for line in fileReader:
     #Fetch the url from file in.txt    
-    page = 0
-    reviewlink = line.strip()    
-    url = reviewlink + str(page)
-    
-    #Opening the url    
-    try: 
-        br.set_handle_refresh(False)
-        response=br.open(url,timeout=(5.0)) 
-    except Exception as e:
-        error_type, error_obj, error_info = sys.exc_info()
-        print 'ERROR FOR URL:',url
-        print error_type, 'Line:', error_info.tb_lineno
-        continue
-    except urllib2.URLError, e:
-        print "Oops, timed out?"
-        pass
-    except socket.timeout:
-        print "Timed out!"
-        pass
-    except:
-        myresponse = []
-        pass
-    
-    html = response.read()
-    bsObj = BeautifulSoup(html)
-    
-    del ReviewList[:]
-    del DateList[:]
-    del RatingList[:]
-    
-    #Will parse the Dates and append it to the list
-    for date in bsObj.findAll("div",{"class":"date line fk-font-small"}):
-        DateList.append(date.text.strip())
-    
-    #Will parse the Reviews and append it to the list
-    for review in bsObj.findAll("span",{"class":"review-text"}):
-        ReviewList.append(review.text.replace('\n', ' ').strip())
+    reviewlink = line.strip()   
+    print reviewlink
 
-    #Will parse the Ratings and append it to the list
-    for rating in bsObj.findAll("div",{"class":"fk-stars"}):
-        RatingList.append(rating['title'].strip())
-
-    #Will check if reviews are available 
-    stop = bsObj.find("div",{"class":"fk-text-center fk-font-big"})
-    if stop:
-        print 'Exiting..No more reviews for this product'
-        break
-    
-    #Calculating the lenghts
-    lenDate = len(DateList)
-    lenReview = len(ReviewList)
-    lenRating = len(RatingList)
-
-    #Comparing length and writing data to file.
-    if lenDate == lenReview == lenRating:
-        index = 0
-        while (index < lenReview):
-            fileWriter.write(sitename + '\t' + ReviewList[index].encode('utf8') +'\t'+ RatingList[index].encode('utf8') + '\t' + DateList[index].encode('utf8')+'\n')
-            index+=1
-    else:
-        print 'Some issue while writing data to file as records all records are not fetched.'
-
-    nextpage = 10
-    while True:
-        newlink = 'http://www.flipkart.com/micromax-32b200hdi-81-cm-32-led-tv/product-reviews/ITMDTZ6NN78YHSZY?pid=TVSDTZ6NN78YHSZY&rating=1,2,3,4,5&reviewers=all&type=top&sort=most_helpful&start=' + str(nextpage)
-        newlink = reviewlink + str(nextpage)    
+    if reviewlink:
+        page=0    
+        while True:
+            url = reviewlink + str(page)
+            #Opening the url    
+            try:
+                response=browser.open(url,timeout=(5.0))
+                time.sleep(2) 
+            except Exception as e:
+                error_type, error_obj, error_info = sys.exc_info()
+                print 'ERROR FOR URL:',url
+                print error_type, 'Line:', error_info.tb_lineno
+                continue
             
-        #Opening the url          
-        try:
-            br.set_handle_refresh(False)            
-            myresponse=br.open(newlink,timeout=(10.0))
-        except Exception as e:
-            error_type, error_obj, error_info = sys.exc_info()
-            print 'ERROR FOR URL:',newlink
-            print error_type, 'Line:', error_info.tb_lineno
-            continue
-        except urllib2.URLError, e:
-            print "Oops, timed out?"
-            pass
-        except socket.timeout:
-            print "Timed out!"
-            pass
-        except:
-            myresponse = []
-            pass
+            #Initializing Beautifulsoup object
+            html = response.read()
+            bsObj = BeautifulSoup(html)
+            
+            for date in bsObj.findAll("div",{"class":"date line fk-font-small"}):
+                DateList.append(date.text.strip())
         
-        myhtml = myresponse.read()
-        bsObj = BeautifulSoup(myhtml)
-        
-        del ReviewList[:]
-        del DateList[:]
-        del RatingList[:]
-
-        #Will parse the Dates and append it to the list
-        for date in bsObj.findAll("div",{"class":"date line fk-font-small"}):
-            DateList.append(date.text.strip())
-
-        #Will parse the Reviews and append it to the list
-        for review in bsObj.findAll("span",{"class":"review-text"}):
-            ReviewList.append(review.text.replace('\n', ' ').strip())
-
-        #Will parse the Ratings and append it to the list        
-        for rating in bsObj.findAll("div",{"class":"fk-stars"}):
-            RatingList.append(rating['title'].strip())
-
-        #Will check if reviews are available         
-        stop = bsObj.find("div",{"class":"fk-text-center fk-font-big"})
-        if stop:
-            print 'Exiting..No more reviews for this product'
-            break
-       
-        nextpage = nextpage + 10
-        print 'Next Page', nextpage
-
-        #Calculating the lenghts
-        lenDate = len(DateList)
-        lenReview = len(ReviewList)
-        lenRating = len(RatingList)
-        
-        #Comparing length and writing data to file.        
-        if lenDate == lenReview == lenRating:
-            index = 0
-            while (index < lenReview):
-                fileWriter.write(sitename + '\t' + ReviewList[index].encode('utf8') +'\t'+ RatingList[index].encode('utf8') + '\t' + DateList[index].encode('utf8')+'\n')
-                index+=1
-        else:
-            print 'Some issue while writing data to file as records all records are not fetched.'
+            #Will parse the Reviews and append it to the list
+            for review in bsObj.findAll("span",{"class":"review-text"}):
+                ReviewList.append(review.text.replace('\n', ' ').strip())
     
+            #Will parse the Ratings and append it to the list
+            for rating in bsObj.findAll("div",{"class":"fk-stars"}):
+                RatingList.append(rating['title'].strip())
+    
+            #Will check if reviews are available 
+            stop = bsObj.find("div",{"class":"fk-text-center fk-font-big"})
+            if stop:
+                print 'Exiting..No more reviews for this product'
+                break
+            
+            page+=10
+            print 'Reviews Collected',len(ReviewList)
+            
+            #Comparing length and writing data to file.            
+            if len(DateList) == len(ReviewList) == len(RatingList):
+                index = 0
+                while (index < len(ReviewList)):
+                    fileWriter.write(website + '\t' + ReviewList[index].encode('utf8') +'\t'+ RatingList[index].encode('utf8') + '\t' + DateList[index].encode('utf8')+'\n')
+                    index+=1
+            else:
+                print 'Some issue while writing data to file as records all records are not fetched.'
+                
+    else:
+        print 'Parsing completed'
+
+print 'Total Reviews Collected',len(ReviewList)
+
 #Printing the running time   
 print time.clock() - start_time, "seconds"
 
